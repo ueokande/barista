f030491e7ba791198b0d0be615c79d643c501db4
Shin'ya Ueoka <ibenza@i-beam.org>
2015-10-20 11:56:01 +0900

    Call platform service in descriptor

--- src/descriptor/service.sh
+++ src/descriptor/service.sh
@@ -1,18 +1,30 @@
-# Control the service as
+# Control the service as following:
 #
 # service 'sshd' $(
-#   action 'ename'
+#   action enable start
 # )
 #
-# is enable sshd service
+# The descriptor enables and starts sshd service.
+
 service() {
   target="$1"
   shift
   declare -A params='('"$@"')'
 
-  [[ -z ${params[name]} ]] || target=${params[name]}
-  [[ -z ${params[action]} ]] && fatal "Parameter \`action\' is required"
+  actions=${params[action]}
 
-  echo service[$target] with ${params[action]}
+  [[ -z ${params[name]} ]] || target=${params[name]}
+  [[ -z $actions ]] && fatal "Parameter \`action\' is required"
+
+  for action in ${actions}; do
+    case $action in
+    enable) service_enable $target;;
+    disable) service_disable $target;;
+    start) service_start $target;;
+    stop) service_stop $target;;
+    reload) service_stop $target;;
+    restart) service_stop $target;;
+    esac
+  done
 }
 declare_resources action name
